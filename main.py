e631b73d58270f109477cdea11c68e227aa9ef57
Junyali <junjia.liu@pm.me>
2024-12-18 14:44:23 +0000

    Add Card shuffling

--- main.py
+++ main.py
@@ -9,6 +9,34 @@ import pygame
 import random
 import timeit
 
+## // Constants \\ ##
+colours = [
+    "red",
+    "yellow",
+    "green",
+    "blue",
+]
+
+values = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "skip",
+    "reverse",
+    "draw_2",
+    "wild",
+    "draw_4",
+]
+
+## // Classes \\ ##
+
 class Player:
 
     def __init__(self, name: str, player_id: int):
@@ -30,10 +58,19 @@ class Human(Player):
 
 class Card:
 
-    def __init__(self, value: str, colour: str, action: str):
+    def __init__(self, value: str, colour: str):
         self.value = value
         self.colour = colour
-        self.action = action
+        # self.action = action
+
+    def get_info(self):
+        return "[{}, {}]".format(self.value.upper(), self.colour.upper())
+
+    def set_colour(self, new_colour: str):
+        self.colour = new_colour
+
+    def set_value(self, new_value: str):
+        self.value = new_value
 
     def can_play(self, top_card):
         return (
@@ -44,6 +81,24 @@ class Card:
 
         )
 
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for colour in colours:
+            for value in values:
+                if value == "wild" or value == "draw_4":
+                    self.deck.append(Card(value, "colourless"))
+                elif value == "0":
+                    self.deck.append(Card(value, colour))
+                else:
+                    for i in range(2):
+                        self.deck.append(Card(value, colour))
+        random.shuffle(self.deck)
+class Game:
+    def __init__(self):
+        self.deck = Deck()
 
 if __name__ == '__main__':
-    print("Hello ICHI!")
+    deck = Deck()
+    for d in deck.deck:
+        print(d.get_info())
